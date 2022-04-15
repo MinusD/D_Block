0bf6851a354bf54316fa0dcf7c7cef33d9901404
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-04-15 21:21:53 +0300

    Modify Task 3

--- Task3.py
+++ Task3.py
@@ -5,14 +5,11 @@ def repl(t):
     return str(int(int(t.group()[:-5]) / 3.6)) + " м/с"
 
 
+def km_to_m(s):
+    return re.sub(r'(\d+) км/ч', repl, s)
+
+
 if __name__ == '__main__':
     data = input()
-    # data = "Шакал разгоняется до скорости 72 км/ч, а гепард до 108 км/ч."
-    pattern = r'(\d+) км/ч'
-    '''pattern4 = r'[0-9]+ км/ч)'
-    pattern3 = r'([0-9]+ км/ч)'
-    pattern2 = r'(\d+ км/ч)'
-    repl2 = r'\1/3.6 м/с'
-    repl1 = r'\n м/с'
-    '''
-    print(re.sub(pattern, repl, data))
+    print(re.sub(r'(\d+) км/ч', repl, data))
+    print(km_to_m(data))
